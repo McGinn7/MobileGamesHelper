197552b2a783a1f70bf8aed8abab563a8a4f2601
mcginn <958032738@qq.com>
2020-07-06 22:50:45 +0800

    feat: add mouse utils

--- base/mouse.py
+++ base/mouse.py
@@ -1,15 +1,43 @@
-def click(handle, xy, noisy_offset=5):
-    pass
+import random
+import time
+
+import win32con
+import win32gui
 
 
-def drag(handle, begin_xy, end_xy):
-    pass
+def _w_param(x, y):
+    return y << 16 | x
+
+
+def click(handle, xy, noisy_offset=8):
+    x, y = int(xy[0]), int(xy[1])
+    if isinstance(noisy_offset, int):
+        x += random.randint(-noisy_offset, noisy_offset)
+        y += random.randint(-noisy_offset, noisy_offset)
+    win32gui.SendMessage(handle, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, _w_param(x, y))
+    time.sleep(0.1)
+    win32gui.SendMessage(handle, win32con.WM_LBUTTONUP, 0, _w_param(x, y))
+
+
+def drag(handle, start_xy, end_xy, speed=20):
+    x0, y0 = int(start_xy[0]), int(start_xy[1])
+    x1, y1 = int(end_xy[0]), int(end_xy[1])
+    win32gui.PostMessage(handle, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, _w_param(x, y))
+    win32gui.SendMessage(handle, win32con.WM_MOUSEMOVE, win32con.MK_LBUTTON, _w_param(x, y))
+    for i in range(speed):
+        time.sleep(0.05)
+        nx = x0 + int((x1 - x0) / speed * (i + 1))
+        ny = y0 + int((y1 - y0) / speed * (i + 1))
+        win32gui.PostMessage(handle, win32con.WM_MOUSEMOVE, win32con.MK_LBUTTON,
+                             _w_param(nx, ny))
+    time.sleep(0.05)
+    win32gui.PostMessage(handle, win32con.WM_LBUTTONUP, 0, _w_param(x1, y1))
 
 
 def scroll(handle):
     pass
 
 
-@property
 def coordinate():
-    pass
+    [x, y] = win32gui.GetCursorPos()
+    return x, y
