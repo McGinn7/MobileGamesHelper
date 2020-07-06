47b562141f9351c9471278fa92c6fee46a77edbf
mcginn <958032738@qq.com>
2020-07-06 23:09:31 +0800

    feat: add template detection

--- base/image.py
+++ base/image.py
@@ -1,2 +1,13 @@
-def detect_img_template():
+import aircv
+
+
+def detect_img_template(im_object, im_test, threshold, findall=False, rgb=True):
+    if findall:
+        return aircv.find_all_template(im_object, im_test, threshold, rgb)
+    else:
+        return aircv.find_template(im_object, im_test, threshold, rgb)
+
+
+# TODO: support object detection by sift
+def detect_img_sift():
     pass
