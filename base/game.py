7f3c4b2db32980edde0203079628dd3d50830d68
mcginn <958032738@qq.com>
2020-07-09 20:49:06 +0800

    feat: load resources

--- base/game.py
+++ base/game.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import json
+import os
 from abc import abstractmethod
 
 import cv2
@@ -17,6 +18,8 @@ class Game:
         self.handle = window.find_handle_by_title_name(self.game_name)
         self.window_size = window.get_window_size(self.handle)
         self.cfg = self.load_config()
+        self.resources = dict()
+        self.load_resources(self.cfg.get('resource_dir'), self.resources)
 
     def load_config(self):
         game2config = json.load(open('../config/game2config.json', 'r', encoding='utf-8'))
@@ -27,8 +30,16 @@ class Game:
             return json.load(open('../config/{}'.format(config_name), 'r', encoding='utf-8'))
         return dict()
 
-    def load_resources(self):
-        pass
+    def load_resources(self, path, _resources, depth=0):
+        for dof in os.listdir(path):
+            path_dof = os.path.join(path, dof)
+            if os.path.isdir(path_dof):
+                _resources[dof] = dict()
+                self.load_resources(path_dof, _resources[dof], depth + 1)
+            else:
+                filename = dof.split('.')[0]
+                abp_dof = os.path.abspath(path_dof)
+                _resources[filename] = abp_dof
 
     @abstractmethod
     def sign_in(self):
